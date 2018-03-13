09a23a63a6c5deb134ea1ee86f24aabdd2841550
Ivan Baksheev <zpix-dev@list.ru>
2018-03-13 22:03:38 +0700

    Second v

---
+++ src/Constants.js
@@ -0,0 +1,53 @@
+export const defaultDice = [
+  {used: false, type: 0},
+  {used: false, type: 0},
+  {used: false, type: 0},
+  {used: false, type: 0},
+  {used: false, type: 0}
+]
+
+export const combinations = [
+  {
+    id: 1,
+    name: 'ones',
+    settings: {
+      type: 'sum',
+      allowed: [1]
+    }
+  }, {
+    id: 2,
+    name: 'twos',
+    settings: {
+      type: 'sum',
+      allowed: [2]
+    }
+  }, {
+    id: 3,
+    name: 'threes',
+    settings: {
+      type: 'sum',
+      allowed: [3]
+    }
+  }, {
+    id: 4,
+    name: 'fours',
+    settings: {
+      type: 'sum',
+      allowed: [4]
+    }
+  }, {
+    id: 5,
+    name: 'fives',
+    settings: {
+      type: 'sum',
+      allowed: [5]
+    }
+  }, {
+    id: 6,
+    name: 'sixes',
+    settings: {
+      type: 'sum',
+      allowed: [6]
+    }
+  }
+]
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -8,14 +8,13 @@
               <th>Co</th>
               <th>1</th>
               <th>2</th>
-              <th>Co</th>
-              <th>1</th>
-              <th>2</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>data</td>
+            <tr v-for="comb in combinations">
+              <td>{{ comb.name }}</td>
+              <td v-on:click="setScore(0, comb.id)">{{ scores[0][comb.id] }}</td>
+              <td v-on:click="setScore(1, comb.id)">{{ scores[1][comb.id] }}</td>
             </tr>
           </tbody>
         </table>
@@ -23,14 +22,10 @@
       <div class="dice block">
         Dices
         <div class="dice-panel">
-          <div class="dice-element">1</div>
-          <div class="dice-element">2</div>
-          <div class="dice-element">3</div>
-          <div class="dice-element">4</div>
-          <div class="dice-element">5</div>
-          <input type="button" id="roll-dice" value="Roll"></input>
+          <div class="dice-element" v-for="d in dice" v-bind:class="{ used: d.used }"
+ v-on:click="d.type != 0 ? d.used = !d.used : ''" >{{ d.type }}</div>
         </div>
-
+        <input v-on:click="roll" type="button" id="roll-dice" v-bind:value="rollButtonMessage"></input>
       </div>
       <!-- <div class="two">Two</div>
       <div class="three">Three</div>
@@ -42,17 +37,61 @@
 </template>
 
 <script>
+import {defaultDice, combinations} from '../Constants'
+
+function getRandomInt (min, max) {
+  var byteArray = new Uint8Array(1)
+  window.crypto.getRandomValues(byteArray)
+
+  var range = max - min + 1
+  var maxRange = 256
+  if (byteArray[0] >= Math.floor(maxRange / range) * range) {
+    return getRandomInt(min, max)
+  }
+  return min + (byteArray[0] % range)
+}
+
 export default {
   name: 'game',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js PWA'
+      playerTurn: 0,
+      rollsLeft: 3,
+      rolled: false,
+      rollButtonMessage: 'Кинуть!',
+      dice: defaultDice,
+      combinations: combinations,
+      scores: [{}, {}]
+    }
+  },
+  methods: {
+    roll: function (event) {
+      if (this.rollsLeft === 1) {
+        this.rollButtonMessage = 'Ход завершен'
+        this.rolled = false
+      } else {
+        this.rolled = true
+
+        for (var i = 0; i < this.dice.length; i++) {
+          var d = this.dice[i]
+          if (!d.used) {
+            d.type = getRandomInt(1, 5)
+          }
+        }
+        this.rollButtonMessage = 'Осталось ' + --this.rollsLeft
+      }
+    },
+    setScore: function (player, combId) {
+      if (player === this.playerTurn) {
+        if (!this.scores[player].hasOwnProperty(combId)) {
+          this.$set(this.scores[player], combId, 1)
+        }
+      }
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 h1, h2 {
   font-weight: normal;
@@ -82,25 +121,36 @@ a {
 .wrapper {
   display: grid;
   grid-template-areas:
-    'dice scores scores'
-    'dice scores scores'
-    'dice scores scores';
-  grid-gap: 20px;
+    'scores scores'
+    'scores scores'
+    'scores scores'
+    'dice dice';
+  grid-gap: 10px;
   grid-auto-rows: minmax(100px, auto);
 }
 
 .dice-panel {
   width: 100%;
-  height: 100%;
-  border-width: 2px;
-  display: table;
+  display: flex;
 }
 
 .dice-element {
-  float: left;
-  background-color: black;
+  margin: 5px;
+  text-align: center;
   width: 20%;
-  padding-bottom: 10%;
+  position: relative; 
+  border: 1px black solid;
+  border-radius: 4px;
+}
+
+.dice-element.used {
+  border: 1px green solid;
+}
+
+.dice-element:before {  /* to make 1:1 aspect ratio; */
+    content:'';
+    padding-top:100%;
+    float:left;
 }
 
 .scores {
@@ -112,14 +162,16 @@ a {
 }
 
 .scores-table {
-    border-collapse: collapse;
-    width: 100%;
-    height: 100%;
+  border-collapse: collapse;
+  width: 100%;
+  height: 100%;
 
 }
 
 .scores-table table, th, td {
-    border: 1px solid black;
+  border: 1px solid black;
 }
+
+
 
 </style>
