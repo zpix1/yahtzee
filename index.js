fdec5ad5a95a2f0dcfe4fcf4f638bd6f7c8ad97e
Ivan Baksheev <zpix-dev@list.ru>
2020-03-12 16:57:45 +0700

    server update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var io = require('socket.io')(process.env.PORT || 3000);
-io.origins('*:*');
+io.set('origins', 'https://zpix1.github.io:80');
+
 var userToSessionCode = {};
 var sessionCodesDB = {};
 
