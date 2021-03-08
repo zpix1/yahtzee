062e001337a79fa13aeb8416c62dd3b4fa9b7adb
Ivan Baksheev <zpix-dev@list.ru>
2021-03-08 16:11:23 +0700

    inline rebuilt

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -97,7 +97,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // copy custom static assets
     new CopyWebpackPlugin([
       {
-        from: path.resolve(__dirname, '../static'),
+        from: path.resolve(__dirname, '../static/'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
